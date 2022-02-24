ee58b6fe44fc295e13206782ad02328e54ee41eb
leoxMar <94301590+leoxMar@users.noreply.github.com>
2022-02-24 20:37:24 +0100

    aggh

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,33 +18,21 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/stilo.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+        <nav class='navbar navbar-expand-lg navbar-light bg-light'>
+            
+       
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
-
-                    </ul>
+               
 
                     <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
+                    <ul class="navbar-nav ">
                         <!-- Authentication Links -->
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                            
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
@@ -76,8 +64,15 @@
         </nav>
 
         <main class="py-4">
+            <!-- Logo -->
+            <div class="flex-shrink-0 flex items-center ms">
+                <a href="{{ route('dashboard') }}">
+                    <x-application-logo class="block h-10 w-auto fill-current text-gray-600" />
+                </a>
+            </div>
             @yield('content')
         </main>
+        
     </div>
 </body>
 </html>
